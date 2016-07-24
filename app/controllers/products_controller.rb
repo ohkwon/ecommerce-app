bf42371ddf8a6e58db0d2029b07df50afd8ef01d
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-24 16:19:37 -0700

    create new and create actions and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,20 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: @id )
   end
 
+  def new
+
+  end
+
+  def create
+    name = params[:name]
+    price = params[:price]
+    image = params[:image]
+    developer = params[:developer]
+    description = params[:description]
+    console = params[:console]
+    rating = params[:rating]
+    @product = Product.new({name: name, price: price, image: image, developer: developer, description: description, console: console, rating: rating})
+    @product.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
   get '/products/:id', to: 'products#show'
+  post '/products', to: 'products#create'
 
 end
