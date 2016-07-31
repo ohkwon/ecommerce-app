5a6f17fdfd0a6830463acb0c69f36fce879eafde
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-31 15:04:09 -0700

    remove image column from products, add new table images, transfer all cover pages to images table

---
+++ db/migrate/20160731214750_addimageurl.rb
@@ -0,0 +1,4 @@
+class Addimageurl < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731205829) do
+ActiveRecord::Schema.define(version: 20160731220145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.decimal  "price",       precision: 6, scale: 2
     t.string   "developer"
-    t.string   "image"
     t.text     "description"
     t.string   "console"
     t.string   "rating"
