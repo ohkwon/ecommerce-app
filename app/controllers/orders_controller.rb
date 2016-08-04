3927e8f202046a80d6eb58cec354309cc9e8ffab
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-03 19:39:32 -0700

    add more options to save prices on orders, edit show page and create show page for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,10 +6,18 @@ class OrdersController < ApplicationController
 
   def create
 
+    product = Product.find_by(id: params[:product][:id])
+    subtotal = product.price * params[:quantity]
+    tax = subtotal * 1.09
+    total = subtotal + tax
+
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product][:id],
-      quantity: params[:quantity]
+      quantity: params[:quantity],
+      subtotal: subtotal,
+      tax: tax,
+      total: total
       )
     if order.save
       flash[:success] = "You successfully made your order!"
@@ -19,6 +27,10 @@ class OrdersController < ApplicationController
       redirect_to "/"
     end
 
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
   end
 
 end
